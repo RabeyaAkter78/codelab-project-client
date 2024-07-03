cad1dc777e1f26250f96424b1c4a4c72012a3d51
RabeyaAkter78 <rabeyariya251@gmail.com>
2024-07-03 18:51:40 +0600

    added who we are

--- src/Components/Shared/SectionTitle/SectionTitle.jsx
+++ src/Components/Shared/SectionTitle/SectionTitle.jsx
@@ -1,9 +1,9 @@
 
 const SectionTitle = ({heading, subHeading}) => {
     return (
-        <div className="my-10 ">
+        <div className="my-10 w-56">
             <h1 className="btn btn-outline bg-white hover:bg-white text-black hover:text-black">{heading}</h1>
-            <p className="font-bold text-3xl text-black ">{subHeading}</p>
+            <p className="font-bold text-3xl text-black my-4">{subHeading}</p>
         </div>
     );
 };
